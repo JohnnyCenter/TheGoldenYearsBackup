c9e9cd2e62d55f7e706c8d53877621785add5625
Simonkjj <simon.jac@hotmail.com>
2021-06-10 00:20:49 +0200

    Small Changes

--- Assets/Player/PlayerRaycast.cs
+++ Assets/Player/PlayerRaycast.cs
@@ -11,7 +11,7 @@ public class PlayerRaycast : MonoBehaviour
     [SerializeField] PlayerInventory p_inventory;
     [SerializeField] PlayerLook p_look;
 
-    [HideInInspector] public GameObject pickup_obj;
+    [SerializeField] public GameObject pickup_obj;
     Transform placePos;
     bool canAddToInventory = false, canPutBack = false;
 
